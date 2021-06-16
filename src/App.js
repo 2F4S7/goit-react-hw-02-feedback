ec21748ea25d948cb38c587955d596150405f981
unknown <nightsta1kerhf@gmail.com>
2021-06-17 02:58:19 +0300

    feedback v0.1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Btn from "./header/btn";
+import "./header/btn.module.css";
+
+function App() {
+  return (
+    <>
+      <Btn title="React Home Work Feedback 02" />
+    </>
+  );
+}
+
+export default App;
