f5c2679a1030cffc5d90fe250d89775c3c6b4423
unknown <nightsta1kerhf@gmail.com>
2021-06-17 08:26:04 +0300

    up

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import HeaderSection from './components/Section/Header/Header';
 import Feedback from './components/Feedback/Feedback';
 import Main from './components/Section/Main/Main';
 import Statistics from './components/Statistics/Statistics';
+import React, { Component } from 'react';
 
-function App() {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
-  return (
-    <>
-      <MyGit title="React Home Work Feedback 02" />
-      <HeaderSection title="Pleaseleave feeedback">
-        <Feedback />
-      </HeaderSection>
-      <Main>
-        <Statistics />
-      </Main>
-    </>
-  );
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <>
+        <MyGit title="React Home Work Feedback 02" />
+        <HeaderSection title="Pleaseleave feeedback">
+          <Feedback />
+        </HeaderSection>
+        <Main>
+          <Statistics />
+        </Main>
+      </>
+    );
+  }
 }
-
-export default App;
